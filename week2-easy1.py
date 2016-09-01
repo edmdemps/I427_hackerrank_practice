5315b9faf6e204823fb6dc8dc7a9ddd9b5e5c48a
edmdemps <edmdemps@indiana.edu>
2016-09-01 12:03:20 -0400

    Add files via upload

---
+++ week2-easy1.py
@@ -0,0 +1,11 @@
+## Arithmetic Operators
+## link: https://www.hackerrank.com/challenges/python-arithmetic-operators
+
+a = eval(input())
+b = eval(input())
+
+print(str(a+b))
+print(str(a-b))
+print(str(a*b))
+
+# I427_hackerrank_practice
